ed1618c88f0197c5f6a12854c192c3a5b34b6bb9
Ryes <33528286+dylanluigi@users.noreply.github.com>
2022-10-13 11:03:43 +0200

    Create guass_column_pivoting.m

---
+++ Metodos Gauss/guass_column_pivoting.m
@@ -0,0 +1,48 @@
+function solution = gauss_column(A, b)
+  n = length(A);
+
+  solution = zeros(n, 0);
+
+  for k = 1:n - 1#For thk k
+    #Loop to find highest value
+      max = k;
+      for i = k + 1:n
+        if abs(A(i, k)) > abs(A(max, k))
+          max = i;
+        endif
+      endfor
+      
+      #We check if (max == 0) statement is true, if so we continue.
+      if A(max, k == 0)
+      continue;
+      endif
+
+      #We permute k value
+      tmp = A(k, :);
+      A(k, :) = A(max, :);
+      A(max, :) = tmp;
+
+      tmp = b(k);
+      b(k) = b(max);
+      b(max) = tmp;
+
+    for i = k + 1:n
+      m = A(i, k) / A(k, k);
+
+      #Gaussian triangulation
+      for j = k:n
+        A(i, j) = A(i, j) - (m * A(k, j));
+      endfor
+
+      b(i) = b(i) - (m * b(k));
+    endfor#k + 1 use to make 0's
+  endfor#k
+  
+  display(A)
+  display(b)
+  
+  solution = upper_triangular_solver(A, b);
+  
+  display(solution)
+
+endfunction
